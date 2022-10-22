15a3651cc3643cc868f7dfcacad9191577eeb680
TiagoBarbosa88 <sombr43@gmail.com>
2022-10-22 12:01:54 -0300

    finalizado

--- src/main.js
+++ src/main.js
@@ -79,9 +79,62 @@ const cardNumberPattern = {
     const foundMask = dynamicMasked.compiledMasks.find(function (item) {
       return number.match(item.regex);
     });
-   
+
     return foundMask;
   },
 };
 
 const cardNumberMasked = IMask(cardNumber, cardNumberPattern);
+
+// Eventos no Button
+
+const addButton = document.getElementById("add-card");
+addButton.addEventListener("click", () => {
+  alert("Cartão adicionado");
+});
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
+// Inserir o conteudo do Input
+
+const cardHolder = document.querySelector("#card-holder");
+cardHolder.addEventListener("input", () => {
+  const ccHolder = document.querySelector(".cc-holder .value");
+  ccHolder.innerText =
+    cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value;
+});
+
+// Eventos no IMask
+
+securityCodeMasked.on("accept", () => {
+  updateSecurityCode(securityCodeMasked.value);
+});
+
+function updateSecurityCode(code) {
+  const ccSecurity = document.querySelector(".cc-security .value");
+
+  ccSecurity.innerText = code.length === 0 ? "123" : code;
+}
+
+cardNumberMasked.on("accept", () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardtype;
+  setCardType(cardType);
+  updateCardNumber(cardNumberMasked.value);
+});
+
+function updateCardNumber(number) {
+  const ccNumber = document.querySelector(".cc-number");
+  ccNumber.innerText = number.length === 0 ? "1234 5678 9012 3456" : number;
+}
+
+expirationDateMasked.on("accept", () => {
+  updateExpirationDate(expirationDateMasked.value);
+});
+
+function updateExpirationDate(date) {
+  const ccExpiration = document.querySelector(".cc-extra .value");
+
+  ccExpiration.innerText = date.length === 0 ? "02/32" : date;
+}
